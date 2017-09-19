4e753c9db422a6f7207087fa9aa0f5686174cade
Michael Aubrey <mcaubrey511@gmail.com>
2017-09-19 14:17:19 +0000

    add translation

--- js/redirect.js
+++ js/redirect.js
@@ -4,7 +4,7 @@ var language = navigator.language || navigator.browserLanguage; //for IE
 if (language) {
     if (language === "ja-JP") {
         // route to ja
-        window.location.replace("http://www.homarezushi.com/en");
+        window.location.replace("http://www.homarezushi.com/ja");
     }
     else {
         // route to en
